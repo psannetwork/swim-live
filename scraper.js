89709a7cf0db9ba8f804c99956461b946e23e41b
psan <127632545+psannetwork@users.noreply.github.com>
2025-07-07 09:51:21 +0000

    new

--- scraper.js
+++ scraper.js
@@ -4,13 +4,12 @@ const { decodeUnicode } = require('./utils');
 async function fetchJson(url) {
   try {
     const res = await axios.get(url);
-    return typeof res.data === 'string' ? JSON.parse(decodeUnicode(res.data)) : res.data;
+    const data = typeof res.data === 'string' ? JSON.parse(decodeUnicode(res.data)) : res.data;
+    return data || [];
   } catch (err) {
     console.error(`Error fetching ${url}:`, err.message);
     return [];
   }
 }
 
-module.exports = {
-  fetchJson
-};
+module.exports = { fetchJson };
